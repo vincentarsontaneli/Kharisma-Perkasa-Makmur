1b73fdbc9eb8838d1619fe4b8f56f58aeb641cd5
Vincent Taneli <vincent.taneli@propella.ai>
2025-07-17 23:39:05 +1000

    try relative path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Kharisma-Perkasa-Makmur/',   
+  base: './',   
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs',                   
